89397fc431f11545e3ecdba6d2b158af44a89825
Tiago Amaro <tiagopadrela@gmail.com>
2016-07-10 14:40:35 +0200

    remove user callbacks

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,3 @@
 class User < ActiveRecord::Base
   validates :database, uniqueness: true, presence: true
-
-  after_create  :create_schema
-  after_destroy :destroy_schema
-
-  private
-
-  def create_schema
-    Apartment::Tenant.create(database)
-  end
-
-  def destroy_schema
-    Apartment::Tenant.drop(database)
-  end
 end
